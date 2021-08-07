3bd334723113b44150323779b9ccf5bc2d3e43cd
Lihor <ukamilove66@gmail.com>
2021-08-07 12:00:14 +0700

    Delete compile-build-date/target directory

--- compile-build-date/target/rls/debug/build/serde-27715c91396e6fd9/build_script_build-27715c91396e6fd9.d
+++
@@ -1,5 +0,0 @@
-/Users/lyhourchhen/Developments/scripts/compile-build-date/target/rls/debug/build/serde-27715c91396e6fd9/build_script_build-27715c91396e6fd9: /Users/lyhourchhen/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.127/build.rs
-
-/Users/lyhourchhen/Developments/scripts/compile-build-date/target/rls/debug/build/serde-27715c91396e6fd9/build_script_build-27715c91396e6fd9.d: /Users/lyhourchhen/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.127/build.rs
-
-/Users/lyhourchhen/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.127/build.rs:
